accd989653719a294cad28e37d07d617e7409bbd
Matias Mancilla <matiassnm.1@gmail.com>
2020-12-08 20:44:47 -0300

    Update docs

--- doc/api_project.js
+++ doc/api_project.js
@@ -1,6 +1,6 @@
 define({
   "title": "Movies API",
-  "url": "http://localhost:3000/api",
+  "url": "https://movies-api-rest.herokuapp.com/api",
   "name": "movies-rest-api",
   "version": "1.0.0",
   "description": "Movies REST API - Node + express",
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-12-08T23:21:12.066Z",
+    "time": "2020-12-08T23:44:38.527Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
